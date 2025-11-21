e18b48a7b7a2ca42e1315a204e40d5378992bcba
BuBuck <sunjeongin123@gmail.com>
2025-11-22 04:26:08 +0900

    fix: StrictMode import 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { AuthProvider } from "./contexts/AuthProvider.jsx";
 
 import App from "./App.jsx";
 
