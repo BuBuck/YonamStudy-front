5c651dc0c183f9f40fb618259533bdda902b92e3
BuBuck <sunjeongin123@gmail.com>
2025-12-02 01:59:48 +0900

    feat: 로그인, 로그아웃 버튼 설정 (임시)

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate("/auth");
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("expiresAt");
+        localStorage.removeItem("expiresToken");
+    };
+
     return (
         <header id="header" role="header">
-            Header
+            <button onClick={handleLogin}>로그인</button>
+            <button onClick={handleLogout}>로그아웃</button>
         </header>
     );
 }
